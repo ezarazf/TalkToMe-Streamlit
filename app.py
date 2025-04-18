0dc7527c053d93f06df227c37a431ade9d6b3a76
ezarazf <70148854+ezarazf@users.noreply.github.com>
2025-04-18 17:06:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ RTC_CONFIGURATION = RTCConfiguration({
     "iceServers": [
         {"urls": ["stun:stun.l.google.com:19302"]},
         {"urls": ["stun:stun1.l.google.com:19302"]}
+    ]
 })
 
 # Setup halaman
